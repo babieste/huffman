06bbcc9c02edf821b695a263684ef27911c9dbf4
Bárbara Este <barbaraeste01@gmail.com>
2019-11-30 00:10:07 -0300

    Initialize build_huffman_tree function - not finished

--- huffman.c
+++ huffman.c
@@ -1,14 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
 
 #define ALPHABET "abcdefghijklmnopqrstuvxyz"
 #define ALPHABET_LENGTH 26
 
+/*
+	This struct represents each character in the frequency table
+*/
 typedef struct {
     char character;
     int char_counter;
     float frequency;
 } node;
+
+/*
+	This struct represents each node in the Huffman tree
+*/
+typedef struct huffmanNode {
+	char character;
+	float frequency;
+	struct huffmanNode *left;
+	struct huffmanNode *right;
+} huffmanNode;
+
+/*
+	This struct represents the Huffman tree
+*/
+typedef struct {
+	int size;
+	huffmanNode *array[25];
+} huffmanTree;
 
 void display_frequency_table(node table[]) {
     int i;
@@ -16,6 +38,16 @@ void display_frequency_table(node table[]) {
         printf("\n--------");
         printf("\nchar \"%c\" - char_counter = %d - char frequency = %f", table[i].character, table[i].char_counter, table[i].frequency);
     }
+}
+
+huffmanNode *create_huffman_node(node char_node) {
+	huffmanNode *huffNode = malloc(sizeof(huffmanNode));
+	
+	huffNode->character = char_node.character;
+	huffNode->frequency = char_node.frequency;
+	huffNode->left = huffNode->right = NULL;
+	
+	return huffNode;
 }
 
 /*
@@ -59,6 +91,36 @@ void read_file(FILE *file, node table[]) {
         float freq = (float) ((float) table[i].char_counter/(float) letter_counter)*100;
         table[i].frequency = freq;
     }
+}
+
+void build_huffman_tree(node table[]) {
+	huffmanTree *tree = malloc(sizeof(huffmanTree));
+	// We can filter the characters table to build the heap nodes
+	// to those characters that were counted in the input.
+	// Firstly, we calculate 'n', which is the amout of leaf nodes
+	// that the Huffman tree will have:
+	int n = 0, i, j = 0;
+	for(i = 0; i < ALPHABET_LENGTH; i++) {
+		if(table[i].char_counter != 0) {
+			n = n + 1;
+		}
+	}
+	
+	tree->size = n;
+	
+	for(i = 0; i < ALPHABET_LENGTH; i++) {
+		if(table[i].char_counter != 0) {
+			// Build a new huffman node based on table character data
+			// and add that node to the tree
+			tree->array[j] = create_huffman_node(table[i]);
+			j = j + 1;
+		}
+	}
+	
+	// Extract min node from tree...
+	
+	
+	
 }
 
 int main() {
